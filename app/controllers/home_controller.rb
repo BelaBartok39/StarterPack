a4674f9d62d4ce1dc0e151ef43ccfc4fca420f02
BelaBartok39 <jackthelion83@gmail.com>
2022-05-03 19:31:10 -0500

    Added Session Control with Login/Logout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,4 @@
 class HomeController < ApplicationController
   def index
-    flash[:notice] = "You are successfully logged in."
-    flash[:alert] = "Invalid password or email."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  get "about" => "about#index"
+
+  get     "about" => "about#index"
+
+  get   "password" => "passwords#edit"
+  patch "password" => "passwords#update"
+
+  get    "sign_up" => "registrations#new"
+  post   "sign_up" => "registrations#create"
+
+  get    "sign_in" => "sessions#new"
+  post   "sign_in" => "sessions#create"
+
+  delete  "logout" => "sessions#destroy"
+
   root "home#index"
 end
