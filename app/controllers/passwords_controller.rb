9d049dfd74c98d23bee2b0c8e8e1436b9540cf88
BelaBartok39 <jackthelion83@gmail.com>
2022-05-04 04:12:09 -0500

    Added Update/Edit Password

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -7,6 +7,8 @@ class PasswordsController < ApplicationController
   def update
     if Current.user.update(password_params)
       redirect_to root_path, notice: "Password Updated"
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
